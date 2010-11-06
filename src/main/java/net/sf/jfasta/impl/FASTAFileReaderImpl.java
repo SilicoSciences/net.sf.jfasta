7c0ea5ab0210b1ba62a9e13bb244dedd56104d77
Kerner <alex.kerner.24@googlemail.com>
2010-11-06 01:47:14 +0100

    JavaDoc.

--- src/main/java/net/sf/jfasta/impl/FASTAFileReaderImpl.java
+++ src/main/java/net/sf/jfasta/impl/FASTAFileReaderImpl.java
@@ -28,25 +28,116 @@ import net.sf.jfasta.FASTAFile;
 import net.sf.jfasta.FASTAFileReader;
 import net.sf.kerner.commons.io.buffered.AbstractBufferedReader;
 
-public class FASTAFileReaderImpl extends AbstractBufferedReader implements FASTAFileReader{
-	
+/**
+ * 
+ * Implementation for {@link net.sf.jfasta.FASTAFileReader FASTAFileReader}.
+ * 
+ * <p>
+ * <b>Example:</b><br>
+ * 
+ * </p>
+ * <p>
+ * 
+ * <pre>
+ * TODO example
+ * </pre>
+ * 
+ * </p>
+ * 
+ * @author <a href="mailto:alex.kerner.24@googlemail.com">Alexander Kerner</a>
+ * @version 2010-11-06
+ * 
+ */
+public class FASTAFileReaderImpl extends AbstractBufferedReader implements
+		FASTAFileReader {
+
+	/**
+	 * A default alphabet for DNA.
+	 * <p>
+	 * { 'A', 'a', 'T', 't','G', 'g', 'C', 'c', 'N', 'n' }
+	 * </p>
+	 */
+	public final static char[] DNA_ALPHABET_IGNORE_CASE = { 'A', 'a', 'T', 't',
+			'G', 'g', 'C', 'c', 'N', 'n' };
+
+	/**
+	 * A default alphabet for DNA.
+	 * <p>
+	 * { 'A', 'a', 'T', 't','G', 'g', 'C', 'c'}
+	 * </p>
+	 */
+	public final static char[] DNA_ALPHABET_IGNORE_CASE_STRICT = { 'A', 'a',
+			'T', 't', 'G', 'g', 'C', 'c' };
+
+	/**
+	 * A default alphabet for DNA.
+	 * <p>
+	 * { 'A', 'T', 'G', 'C', 'N' }
+	 * </p>
+	 */
+	public final static char[] DNA_ALPHABET = { 'A', 'T', 'G', 'C', 'N' };
+
+	/**
+	 * A default alphabet for DNA.
+	 * <p>
+	 * { 'A', 'T', 'G', 'C' }
+	 * </p>
+	 */
+	public final static char[] DNA_ALPHABET_STRICT = { 'A', 'T', 'G', 'C' };
+
+	/**
+	 * Alphabet that is used for sequence validity checking. If null, sequence
+	 * is not validated.
+	 */
 	protected final char[] alphabet;
 
+	/**
+	 * 
+	 * Build a new {@code FASTAFileReaderImpl}. this {@code FASTAFileReaderImpl}
+	 * will not perform any sequence validation.
+	 * 
+	 * @param reader
+	 *            reader from which stream is read
+	 */
 	public FASTAFileReaderImpl(BufferedReader reader) {
 		super(reader);
 		this.alphabet = null;
 	}
 
+	/**
+	 * 
+	 * Build a new {@code FASTAFileReaderImpl}. this {@code FASTAFileReaderImpl}
+	 * will not perform any sequence validation.
+	 * 
+	 * @param file
+	 *            file from which stream is read
+	 */
 	public FASTAFileReaderImpl(File file) throws IOException {
 		super(file);
 		this.alphabet = null;
 	}
 
+	/**
+	 * 
+	 * Build a new {@code FASTAFileReaderImpl}. this {@code FASTAFileReaderImpl}
+	 * will not perform any sequence validation.
+	 * 
+	 * @param file
+	 *            stream from which is read
+	 */
 	public FASTAFileReaderImpl(InputStream stream) {
 		super(stream);
 		this.alphabet = null;
 	}
-	
+
+	/**
+	 * 
+	 * Build a new {@code FASTAFileReaderImpl}. this {@code FASTAFileReaderImpl}
+	 * will not perform any sequence validation.
+	 * 
+	 * @param reader
+	 *            reader from which stream is read
+	 */
 	public FASTAFileReaderImpl(Reader reader) {
 		super(reader);
 		this.alphabet = null;
@@ -56,7 +147,7 @@ public class FASTAFileReaderImpl extends AbstractBufferedReader implements FASTA
 		super(reader);
 		this.alphabet = alphabet;
 	}
-	
+
 	public FASTAFileReaderImpl(BufferedReader reader, char[] alphabet) {
 		super(reader);
 		this.alphabet = alphabet;
@@ -72,8 +163,6 @@ public class FASTAFileReaderImpl extends AbstractBufferedReader implements FASTA
 		this.alphabet = alphabet;
 	}
 
-	
-
 	public FASTAElementIterator getIterator() throws IOException {
 		return new FASTAElementIterator(super.reader, alphabet);
 	}
@@ -81,13 +170,12 @@ public class FASTAFileReaderImpl extends AbstractBufferedReader implements FASTA
 	public FASTAFile read() throws IOException {
 		final Collection<FASTAElement> result = new LinkedHashSet<FASTAElement>();
 		FASTAElementIterator it = getIterator();
-		while(it.hasNext()){
+		while (it.hasNext()) {
 			result.add(it.next());
-//			System.err.println("result now " + result);
+			// System.err.println("result now " + result);
 		}
 		it.close();
 		return new FASTAFileImpl(result);
 	}
 
-		
 }
