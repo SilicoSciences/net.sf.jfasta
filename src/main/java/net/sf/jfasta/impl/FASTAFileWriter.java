cb8be03b1be6a03012ab5bc8181432c38f1c4a5c
Kerner <alex.kerner.24@googlemail.com>
2010-11-30 20:16:57 +0100

    [API-break] FASTAFileWriter now implements ObjectWriter<FASTAElement> instead of ObjectWriter<FASTAFile>. This should encourage to write a FASTAFile element by element. JavaDoc.

--- src/main/java/net/sf/jfasta/impl/FASTAFileWriter.java
+++ src/main/java/net/sf/jfasta/impl/FASTAFileWriter.java
@@ -25,24 +25,101 @@ import net.sf.jfasta.FASTAFile;
 import net.sf.kerner.utils.io.ObjectWriter;
 import net.sf.kerner.utils.io.buffered.AbstractBufferedWriter;
 
-public class FASTAFileWriter extends AbstractBufferedWriter implements ObjectWriter<FASTAFile> {
+/**
+ * 
+ * A {@code FASTAFileWriter} writes {@link net.sf.jfasta.FASTAElement
+ * FASTAElement}s and {@link net.sf.jfasta.FASTAFile FASTAFile}s to
+ * <ul>
+ * <li>
+ * a {@link java.io.File}</li>
+ * <li>
+ * a {@link java.io.Writer}</li>
+ * <li>
+ * an {@link java.io.OutputStream}</li>
+ * </ul>
+ * 
+ * <p>
+ * <b>Example:</b><br>
+ * 
+ * </p>
+ * <p>
+ * 
+ * <pre>
+ * TODO example
+ * </pre>
+ * 
+ * </p>
+ * 
+ * <p>
+ * {@code FASTAFileWriter} is thread save.
+ * </p>
+ * 
+ * @author <a href="mailto:alex.kerner.24@googlemail.com">Alexander Kerner</a>
+ * @version 2010-11-30
+ * 
+ */
+public class FASTAFileWriter extends AbstractBufferedWriter implements
+		ObjectWriter<FASTAElement> {
 
+	/**
+	 * 
+	 * Create a {@code FASTAFileWriter} that writes to a file.
+	 * 
+	 * @param file
+	 *            file to write to
+	 * @throws IOException
+	 *             if file is not accessible
+	 */
 	public FASTAFileWriter(File file) throws IOException {
 		super(file);
 	}
 
+	/**
+	 * 
+	 * Create a {@code FASTAFileWriter} that writes to a stream.
+	 * 
+	 * @param stream
+	 *            output stream to write to
+	 */
 	public FASTAFileWriter(OutputStream stream) {
 		super(stream);
 	}
 
+	/**
+	 * 
+	 * Create a {@code FASTAFileWriter} that writes to a writer.
+	 * 
+	 * @param writer
+	 *            writer to write to
+	 */
 	public FASTAFileWriter(Writer writer) {
 		super(writer);
 	}
 
+	/**
+	 * 
+	 * Write a {@link net.sf.jfasta.FASTAFile FASTAFile} at once.
+	 * <p>
+	 * <b> Note:</b> For big FASTA files, use {@link #write(FASTAElement)}
+	 * instead and write FASTA file element by element.
+	 * </p>
+	 * 
+	 * @param e
+	 *            FASTA file to write
+	 * @throws IOException
+	 *             if writing fails
+	 */
 	public synchronized void write(FASTAFile e) throws IOException {
 		super.writer.write(e.toString());
 	}
-	
+
+	/**
+	 * Write a {@link net.sf.jfasta.FASTAElement FASTAElement}.
+	 * <p>
+	 * <b>Note:</b> For big FASTA elements, prepare for equivalent memory usage.
+	 * </p>
+	 * 
+	 */
 	public synchronized void write(FASTAElement e) throws IOException {
 		super.writer.write(e.toString());
 	}
